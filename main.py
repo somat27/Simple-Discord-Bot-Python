285396688e62b7e570603ab5c53cbdbfc45e6f26
somat27 <70666298+somat27@users.noreply.github.com>
2021-08-14 18:30:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 token = "YOUR_DISCORD_BOT_TOKEN"
 prefix = "YOUR_PREFIX"
 
-#--- BOT ---
-import keep_alive
+#--- DEPENDENCIAS ---
+from keep_alive import keep_alive
 import discord
 from discord.ext import commands
 from discord import Member
@@ -11,64 +11,37 @@ import requests
 import json
 import os
 import re
-
+from datetime import datetime
+import urllib
 
 print("[Ultra's Bot] Carregado todas as dependencias!")
+
+#--- BOT ---
 
 bot = commands.Bot(command_prefix=prefix)
 bot.remove_command("help")
 
+
 @bot.event
 async def on_ready():
-    print("\n\n[Ultra's Bot] Pronto! Estou a espera de comandos..")
-    print(f"[Ultra's Bot] Logado em: {bot.user.name}")
-    print(f"[Ultra's Bot] ID: {bot.user.id}\n\n")
-    await bot.change_presence(
-        activity=discord.Streaming(name='Biana di Drill by sandrini9k', url='https://www.twitch.tv/Ultra_Somat27'))
+	print("\n\n[Ultra's Bot] Pronto! Estou a espera de comandos..")
+	print(f"[Ultra's Bot] Logado em: {bot.user.name}")
+	print(f"[Ultra's Bot] ID: {bot.user.id}\n\n")
+	await bot.change_presence(
+	    activity=discord.Streaming(name='Biana di Drill by sandrini9k',
+	                               url='https://www.twitch.tv/Ultra_Somat27'))
+
 
 #--- COMANDOS ---
 
-@bot.command()
-@commands.has_permissions(manage_messages=True)
-async def clear(ctx, amount=200):
-    if amount > 200:
-        await ctx.send(
-            f"{ctx.author.mention} So podes apagar até 200 mensagens!")
-        return
-    else:
-        await ctx.channel.purge(limit=amount)
-        await ctx.send(f"Limpaste {amount} mensagens!", delete_after=5)
+from CMDS import music, info, hacking, mod
 
-@bot.command()
-async def avatar(ctx, member: Member = None):
-    if not member:
-        member = ctx.author
-    await ctx.send('Logo de: ' + member.name)
-    await ctx.send(member.avatar_url)
+cogs = [music, info, hacking, mod]
 
-@bot.command()
-async def btc(ctx):
-        url = f"https://api.coindesk.com/v1/bpi/currentprice.json"
-        r = requests.get(url)
-        re = r.json()
-        price_usd = re["bpi"]["USD"]["rate"]
-        price_eur = re["bpi"]["EUR"]["rate"]
-        disclaimer = re["disclaimer"]
-        update = re["time"]["updated"]
-        embed = discord.Embed(title=f"Preço da **Bitcoin**", description=disclaimer, color=0xffff00)
-        embed.add_field(name="USD", value=f"** {price_usd} $**")
-        embed.add_field(name="EUR", value=f"** {price_eur} €**")
-        embed.set_footer(text=f"Data: {update}")
-        await ctx.send(embed=embed)
-
-@bot.command()
-async def ip(ctx, arg):
-    res = requests.get(f"https://ipinfo.io/{arg}")
-    data = res.json()
-    pretty_data = json.dumps(data, indent=4)
-    await ctx.send(f"```json\n{pretty_data}\n```")
+for i in range(len(cogs)):
+	cogs[i].setup(bot)
 
 #--- MANTER O BOT 24/7 ---
 
-keep_alive.keep_alive()
+keep_alive()
 bot.run(token)
